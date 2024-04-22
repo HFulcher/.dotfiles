0a704ff5a880bd73aa4377b9152a9c45e09a6a8d
Huw Fulcher <fulcherhuw@gmail.com>
2024-04-22 10:29:14 +0100

    Some extras

--- nvim/lua/huw/remap.lua
+++ nvim/lua/huw/remap.lua
@@ -33,3 +33,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+
+-- Better indenting 
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
